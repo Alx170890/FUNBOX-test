a97e14a0df2c1f0719d3cb044995763498eeec2e
Alexey <Alx170890@gmail.com>
2020-09-17 10:37:45 +0300

    Updates

---
+++ precache-manifest.689abe9be43f424c13cdd3f09a8c01d1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebb5870f181af923eb06",
+    "url": "/funbox-test/static/css/main.ceaade66.chunk.css"
+  },
+  {
+    "revision": "ebb5870f181af923eb06",
+    "url": "/funbox-test/static/js/main.d9aa079d.chunk.js"
+  },
+  {
+    "revision": "47f57e815f3d607c1022",
+    "url": "/funbox-test/static/js/runtime~main.fc5ff06a.js"
+  },
+  {
+    "revision": "f5a0adc8d0bc3fbf7243",
+    "url": "/funbox-test/static/js/2.5145a938.chunk.js"
+  },
+  {
+    "revision": "8c2bc10e0121e15da6a50d6a5f31dbbe",
+    "url": "/funbox-test/static/media/Cat.8c2bc10e.png"
+  },
+  {
+    "revision": "44bde8744ccc0de7995a23b0e82dd281",
+    "url": "/funbox-test/static/media/Exo2.0-Thin.44bde874.otf"
+  },
+  {
+    "revision": "cf966899676e5139485b16bc59fb26c1",
+    "url": "/funbox-test/static/media/Background.cf966899.jpg"
+  },
+  {
+    "revision": "a95539e4959af837c24878ab14add61e",
+    "url": "/funbox-test/index.html"
+  }
+];
